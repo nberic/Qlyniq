c10ac3d3f0a22ed5e0e756cff063f2ad88a99342
nberic <ct.nberic@gmail.com>
2019-10-02 20:36:14 +0200

    Added modifying attirbutes to the `Patient` model.

--- Qlyniq/Models/Patients.cs
+++ Qlyniq/Models/Patients.cs
@@ -16,21 +16,38 @@ namespace Qlyniq.Models
             Labreports = new HashSet<Labreports>();
         }
 
+        [Key]
         public uint Id { get; set; }
+
         [Required]
+        [Display(Name = "Social Security Number")]
+        [StringLength(13, MinimumLength = 13)]
+        [DisplayFormat(ConvertEmptyStringToNull = true, NullDisplayText = "Empty")]
+        [RegularExpression(@"^[0-9]{13}$", ErrorMessage = "Please input a sequence of 13 digits.")]
         [Column(TypeName = "varchar(13)")]
         public string SocialSecurityNumber { get; set; }
+
         [Required]
+        [Display(Name = "First Name")]
         [Column(TypeName = "varchar(255)")]
         public string FirstName { get; set; }
+
         [Required]
+        [Display(Name = "Last Name")]
         [Column(TypeName = "varchar(255)")]
         public string LastName { get; set; }
+
         [Column(TypeName = "date")]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
         public DateTime? BirthDate { get; set; }
+
         [Required]
+        [RegularExpression(@"^Male$|^Female$", ErrorMessage = "Only valid values for the Gender field are 'Male' and 'Female'")]
         [Column(TypeName = "enum('Male','Female')")]
         public string Gender { get; set; }
+
+        [Display(Name = "Healthcare Provider")]
         [Column(TypeName = "varchar(100)")]
         public string HealthCareProvider { get; set; }
 
