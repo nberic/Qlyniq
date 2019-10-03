5d834045775e1fcfef5ca9b27adb88c91a4325b9
Nemanja Berić <nemanja.beric@comtrade.com>
2019-10-03 15:59:45 +0200

    Added attributes to properties of the Office model.

---
+++ Qlyniq/Models/Offices.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Qlyniq.Models
+{
+    [Table("offices")]
+    public partial class Offices
+    {
+        public Offices()
+        {
+            EmployeesDeanOffice = new HashSet<Employees>();
+            EmployeesOffice = new HashSet<Employees>();
+        }
+
+        [Key]
+        public uint Id { get; set; }
+
+        [Display(Name = "Office Name")]
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public string Name { get; set; }
+
+        public uint? OfficeNumber { get; set; }
+
+        [Column(TypeName = "decimal(10,0)")]
+        public decimal Budget { get; set; } = 0.0M;
+
+
+        [InverseProperty("DeanOffice")]
+        public virtual ICollection<Employees> EmployeesDeanOffice { get; set; }
+        [InverseProperty("Office")]
+        public virtual ICollection<Employees> EmployeesOffice { get; set; }
+    }
+}
