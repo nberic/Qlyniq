73d11278a7aa9deb3ccb33bba7c38d487398443c
Nemanja Berić <nemanja.beric@comtrade.com>
2019-10-03 16:05:09 +0200

    Added attributes to the Appointments model.

---
+++ Qlyniq/Models/Appointments.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Qlyniq.Models
+{
+    [Table("appointments")]
+    public partial class Appointments
+    {
+        [Key]
+        public uint Id { get; set; }
+
+        [Display(Name = "First Name")]
+        [DisplayFormat(ConvertEmptyStringToNull = true, NullDisplayText = "Unknown")]
+        [Column(TypeName = "varchar(255)")]
+        public string PatientFirstName { get; set; } = null;
+
+        [Display(Name = "Last Name")]
+        [DisplayFormat(ConvertEmptyStringToNull = true, NullDisplayText = "Unknown")]
+        [Column(TypeName = "varchar(255)")]
+        public string PatientLastName { get; set; }
+        
+        public uint? PatientId { get; set; }
+
+        [Display(Name = "Appointed Doctor")]
+        public uint DoctorId { get; set; }
+
+        [Required]
+        [Display(Name = "Starting Time")]
+        [Column(TypeName = "datetime")]
+        public DateTime StartingTime { get; set; }
+        
+
+        [ForeignKey("DoctorId")]
+        [InverseProperty("Appointments")]
+        public virtual Employees Doctor { get; set; }
+        [ForeignKey("PatientId")]
+        [InverseProperty("Appointments")]
+        public virtual Patients Patient { get; set; }
+    }
+}
